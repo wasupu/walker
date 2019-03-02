dcf5eec069d60e6caedd42114afa58dda6ec2e55
Raimundo Alegria <raimundoangelalegria@bbva.com>
2019-03-02 23:31:49 +0100

    Start the sensors

--- wasupu_walker/__main__.py
+++ wasupu_walker/__main__.py
@@ -14,22 +14,31 @@ def main():
     sound = Sound()
     sound.speak('Start program')
 
-    front_ultrasonic_sensor_scheduler = sched.scheduler(time.time, time.sleep)
-    front_ultrasonic_sensor_scheduler \
-        .enter(1, 1,
-               check_front_distance,
-               (front_ultrasonic_sensor_scheduler,))
-
-    back_ultrasonic_sensor_scheduler = sched.scheduler(time.time, time.sleep)
-    back_ultrasonic_sensor_scheduler \
-        .enter(1, 1,
-               check_back_distance,
-               (back_ultrasonic_sensor,))
+    start_front_ultrasonic_sensor()
+    start_back_ultrasonic_sensor()
 
     go_ahead()
 
     time.sleep(30)
     tank_drive.stop()
+
+
+def start_back_ultrasonic_sensor():
+    back_ultrasonic_sensor_scheduler = sched.scheduler(time.time, time.sleep)
+    back_ultrasonic_sensor_scheduler \
+        .enter(1, 1,
+               check_back_distance,
+               (back_ultrasonic_sensor,))
+    back_ultrasonic_sensor_scheduler.run()
+
+
+def start_front_ultrasonic_sensor():
+    front_ultrasonic_sensor_scheduler = sched.scheduler(time.time, time.sleep)
+    front_ultrasonic_sensor_scheduler \
+        .enter(1, 1,
+               check_front_distance,
+               (front_ultrasonic_sensor_scheduler,))
+    front_ultrasonic_sensor_scheduler.run()
 
 
 def check_front_distance(front_ultrasonic_sensor_scheduler):
