9dd3f2e46fb7788ed06c23142a4d54edea14b9ed
Raimundo Alegria <raimundoangelalegria@bbva.com>
2019-03-02 22:05:55 +0100

    Add speed

--- wasupu_walker/__main__.py
+++ wasupu_walker/__main__.py
@@ -9,7 +9,7 @@ def main():
     sound.speak('Welcome to the E V 3 dev project!')
 
     tank_drive = MoveTank(OUTPUT_A, OUTPUT_B, motor_class=ev3dev2.motor.MediumMotor)
-    tank_drive.on_for_seconds(SpeedPercent(100), SpeedPercent(-100), 5)
+    tank_drive.on_for_seconds(SpeedPercent(-150), SpeedPercent(150), 5)
 
 
 main()
